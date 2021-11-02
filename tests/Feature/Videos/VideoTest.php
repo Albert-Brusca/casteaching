292cb8b70f25cd8c799fa69a032428eded2da329
Albert-Brusca <abrusca@iesebre.com>
2021-11-02 16:08:22 +0100

    Primer test -> ROIG

---
+++ tests/Feature/Videos/VideoTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Videos;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VideoTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @test
+     */
+    public function users_can_view_videos()
+    {
+
+        // FASE 1 -> Preparació -> Prepare
+        // WISHFUL PROGRAMMING -> API
+        $video = Video::create([
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+            'completed' => false,
+            'previous' => null,
+            'next' => null,
+            'series_id' => 1,
+        ]);
+
+        // FASE 2 -> Execució -> Executa el codi a provar
+
+        $response = $this->get('/videos/1' . $video->id);
+
+        // FASE 3 -> Assertions -> comprovacions
+        $response->assertStatus(200);
+        $response->assertSee('Ubuntu 101');
+
+    }
+}
