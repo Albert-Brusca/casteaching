8b22f8e8af8ec7ec235d0c31a04d30093fd87701
Albert-Brusca <abrusca@iesebre.com>
2021-11-04 15:45:44 +0100

    Primer test VERD

--- tests/Feature/Videos/VideoTest.php
+++ tests/Feature/Videos/VideoTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature\Videos;
 
+use App\Models\Video;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class VideoTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -16,6 +19,7 @@ class VideoTest extends TestCase
     public function users_can_view_videos()
     {
 
+
         // FASE 1 -> Preparació -> Prepare
         // WISHFUL PROGRAMMING -> API
         $video = Video::create([
@@ -23,19 +27,28 @@ class VideoTest extends TestCase
             'description' => '# Here description',
             'url' => 'https://youtu.be/w8j07_DBl_I',
             'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
-            'completed' => false,
             'previous' => null,
             'next' => null,
-            'series_id' => 1,
+            'series_id' => 1
+        ]);
+        $video2 = Video::create([
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+            'previous' => null,
+            'next' => null,
+            'series_id' => 1
         ]);
 
         // FASE 2 -> Execució -> Executa el codi a provar
-
-        $response = $this->get('/videos/1' . $video->id);
+        $response = $this->get('/videos/' . $video->id);
 
         // FASE 3 -> Assertions -> comprovacions
         $response->assertStatus(200);
         $response->assertSee('Ubuntu 101');
+        $response->assertSee('Here description');
+        $response->assertSee('December 13');
 
     }
 }
