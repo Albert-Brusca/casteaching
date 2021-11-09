f62ceaf3d5eb9eb576cafeee479aa616dfc9ddb3
Albert-Brusca <abrusca@iesebre.com>
2021-11-09 16:36:21 +0100

    Resultat video 107. Correcció exercicis i Testos unitaris, Funcions helpers i preparació base de dades

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,29 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        \App\Models\User::factory(10)->create();
+        create_default_user();
 
-        User::create([
-            'name' => Hash::make(config('casteaching.default_user.name')),
-            'email' => Hash::make(config('casteaching.default_user.email')),
-            'password' => Hash::make(config('casteaching.default_user.password'))
-        ]);
-
-        User::create([
-            'name' => Hash::make(config('casteaching.default_user_profe.name')),
-            'email' => Hash::make(config('casteaching.default_user_profe.email')),
-            'password' => Hash::make(config('casteaching.default_user_profe.password'))
-        ]);
-
-        //Refactorització -> Helpers -> create_default_videos() -> Helpers -> PHP -> Composer.json -> autoload
-        Video::create([
-            'title' => 'Ubuntu 101',
-            'description' => '# Here description',
-            'url' => 'https://youtu.be/w8j07_DBl_I',
-            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
-            'previous' => null,
-            'next' => null,
-            'series_id' => 1
-        ]);
+        create_default_videos();
     }
 }
