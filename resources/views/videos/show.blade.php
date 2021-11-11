7121f709a7197a0d6a62fe4d3a50ad7498251ae8
Albert-Brusca <abrusca@iesebre.com>
2021-11-11 16:45:27 +0100

    Aplicar estils des de zero

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,7 +1,19 @@
 <x-casteaching-layout>
-    <p>{{ $video->title }}</p>
-    <ul>
-        <li>Description: {{ $video->description }}</li>
-        <li>Data: {{ $video->published_at->format('F d') }}</li>
-    </ul>
+    <div class="flex flex-col h-screen space-x-4 space-y-4 lg:space-x-4 lg:space-y-4 xl:space-x-10 xl:space-y-5
+        2xl:space-x-20 2xl:space-y-10">
+        <iframe
+            class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5"
+            src="https://www.youtube.com/embed/ednlsVl-NHA"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+
+        <h2 class="text-gray-900 uppercase font-bold text-2xl tracking-tight">
+            {{ $video->title }}
+        </h2>
+        <div>
+            {{ $video->description }}
+        </div>
+
+    </div>
 </x-casteaching-layout>
