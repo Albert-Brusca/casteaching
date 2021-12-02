3f2e0b6ac010210a4ba26dc9a176beaba1b52d32
Albert-Brusca <abrusca@iesebre.com>
2021-12-02 16:51:22 +0100

    Form per a crear usuaris + destroy usuaris

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,11 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         ->name('manage.videos');
 
     Route::post('/manage/videos',[ VideosManageController::class,'store' ]);
-    Route::delete('/manage/videos/{id}',[ VideosManageController::class,'destroy' ])->middleware(['can:videos_manage_index']);
+    Route::delete('/manage/videos/{id}',[ VideosManageController::class,'destroy' ])->middleware(['can:videos_manage_destroy']);
 
     Route::get('/manage/users', [ UsersManageController::class,'index'])->middleware(['can:users_manage_index'])
         ->name('manage.users');
+
+    Route::post('/manage/users',[ UsersManageController::class,'store' ])->middleware(['can:users_manage_store']);
+    Route::delete('/manage/users/{id}',[ UsersManageController::class,'destroy' ])->middleware(['can:users_manage_destroy']);
 });
