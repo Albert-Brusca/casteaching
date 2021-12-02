b8fb035d5cb041f2c40f54a4a6e9f861852118c2
Albert-Brusca <abrusca@iesebre.com>
2021-12-02 16:28:12 +0100

    Alguns canvis d'estil

--- resources/views/users/manage/index.blade.php
+++ resources/views/users/manage/index.blade.php
@@ -1,4 +1,9 @@
 <x-casteaching-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
     <div class="flex flex-col mt-10">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
