94153d3757349f023b97cd919c1ddc0586ee1997
Albert-Brusca <abrusca@iesebre.com>
2021-12-02 17:22:35 +0100

    121. + Laravel debugbar, laravel Telescope

--- resources/views/users/manage/index.blade.php
+++ resources/views/users/manage/index.blade.php
@@ -105,7 +105,11 @@
                         <tbody>
                         <!-- Odd row -->
                         @foreach($users as $user)
-                            <tr class="bg-white">
+                            @if($loop->odd)
+                                <tr class="bg-white">
+                            @else
+                                <tr class="bg-gray">
+                            @endif
                                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                     {{ $user->id }}
                                 </td>
--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -111,7 +111,11 @@
                         <tbody>
                         <!-- Odd row -->
                         @foreach($videos as $video)
-                            <tr class="bg-white">
+                            @if($loop->odd)
+                                <tr class="bg-white">
+                            @else
+                                <tr class="bg-gray">
+                            @endif
                                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                     {{ $video->id }}
                                 </td>
