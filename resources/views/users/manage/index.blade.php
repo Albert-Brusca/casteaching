d87a0ceb8a49e2c58d4712a7c2605c25a2327d5c
Albert-Brusca <abrusca@iesebre.com>
2022-02-10 18:55:20 +0100

    136. Utilitzant jetStream per als formularis

--- resources/views/users/manage/index.blade.php
+++ resources/views/users/manage/index.blade.php
@@ -9,69 +9,45 @@
             <x-status></x-status>
         @can('users_manage_create')
 
-                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div class="md:grid md:grid-cols-3 md:gap-6 bg-white md:bg-transparent">
-                            <div class="md:col-span-1">
-                                <div class="px-4 py-4 sm:px-6 md:px-4">
-                                    <h3 class="text-lg font-medium leading-6 text-gray-900">Usuari</h3>
-                                    <p class="mt-1 text-sm text-gray-600">
-                                        Informació bàsica de l'usuari
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="mt-3 md:mt-0 md:col-span-2">
-                                <form data-qa="form_user_create" action="#" method="POST" >
-                                    @csrf
-                                    <div class="shadow sm:rounded-md sm:overflow-hidden md:bg-white">
-                                        <div class="px-4 py-5 space-y-6 sm:p-6">
 
-                                            <div>
-                                                <label for="name" class="block text-sm font-medium text-gray-700">
-                                                    Name
-                                                </label>
-                                                <div class="mt-1">
-                                                    <input required id="name" type="text" name="name" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2" placeholder="Nom del usuari"></input>
-                                                </div>
-                                                <p class="mt-2 text-sm text-gray-500">
-                                                    Nom de l'usuari
-                                                </p>
-                                            </div>
+                <x-jet-form-section>
+                    <x-slot name="title">
+                        {{ __('Users') }}
+                    </x-slot>
 
-                                            <div>
-                                                <label for="email" class="block text-sm font-medium text-gray-700">
-                                                    Email
-                                                </label>
-                                                <div class="mt-1">
-                                                    <input required id="email" type="email" name="email" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="pepepardo@jeans.com"></input>
-                                                </div>
-                                                <p class="mt-2 text-sm text-gray-500">
-                                                    Email
-                                                </p>
-                                            </div>
-                                            <div class="grid grid-cols-3 gap-6">
-                                                <div class="col-span-3">
-                                                    <label for="password" class="block text-sm font-medium text-gray-700">
-                                                        Password
-                                                    </label>
-                                                    <div class="mt-1 flex rounded-md shadow-sm">
-                                                        <input required type="password" name="password" id="password" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300" placeholder="">
-                                                    </div>
-                                                </div>
-                                            </div>
+                    <x-slot name="description">
+                        {{ __('Informació basica del usuari.') }}
+                    </x-slot>
 
-                                        </div>
-                                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                Crear
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
+                    <x-slot name="actions">
+                        <x-jet-button>
+                            {{ __('Save') }}
+                        </x-jet-button>
+                    </x-slot>
+
+                    <x-slot name="form">
+                        @csrf
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-jet-label for="name" value="{{ __('Name') }}" />
+                            <input required id="name" type="text" name="name" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2" placeholder="Nom del usuari"></input>
+                            <x-jet-input-error for="name" class="mt-2" />
                         </div>
-                    </div>
-                </div>
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-jet-label for="email" value="{{ __('Email') }}" />
+                            <input required id="email" type="email" name="email" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="pepepardo@jeans.com"></input>
+                            <x-jet-input-error for="email" class="mt-2" />
+                        </div>
+                        <div class="col-span-6 sm:col-span-4">
+                            <x-jet-label for="password" value="{{ __('Password') }}" />
+                            <input required type="password" name="password" id="password" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300" placeholder="">
+                            <x-jet-input-error for="password" class="mt-2" />
+                        </div>
+
+                    </x-slot>
+                </x-jet-form-section>
+                <x-jet-section-border/>
+
+
 
             @endcan
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -145,6 +121,6 @@
                 </div>
             </div>
         </div>
+        </div>
     </div>
-
 </x-casteaching-layout>
--- resources/views/videos/manage/edit.blade.php
+++ resources/views/videos/manage/edit.blade.php
@@ -7,76 +7,52 @@
     <div class="mx-auto sm:px-6 lg:px-8 w-full max-w-7xl">
 
         @can('videos_manage_create')
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mb-1 md:mb-2 lg:mb-4">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div class="md:grid md:grid-cols-3 md:gap-6 bg-white md:bg-transparent">
-                        <div class="md:col-span-1">
-                            <div class="px-4 py-4 sm:px-6 md:px-4">
-                                <h3 class="text-lg font-medium leading-6 text-gray-900">Vídeos</h3>
-                                <p class="mt-1 text-sm text-gray-600">
-                                    Informació bàsica del vídeo
-                                </p>
-                            </div>
-                        </div>
-                        <div class="md:mt-0 md:col-span-2">
-                            <form data-qa="form_video_edit" action="#" method="POST" >
-                                @csrf
-                                @method('PUT')
-                                <div class="shadow sm:rounded-md sm:overflow-hidden md:bg-white">
-                                    <div class="px-4 py-5 space-y-6 sm:p-6">
 
-                                        <div>
-                                            <label for="title" class="block text-sm font-medium text-gray-700">
-                                                Title
-                                            </label>
-                                            <div class="mt-1">
-                                                <input required type="text" id="title" name="title" rows="3" class="shadow-sm mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2" value="{{ $video->title }}"></input>
-                                            </div>
-                                            <p class="mt-2 text-sm text-gray-500">
-                                                Titol curt del vídeo
-                                            </p>
-                                        </div>
+            <x-jet-form-section>
+                <x-slot name="title">
+                    {{ __('Videos') }}
+                </x-slot>
 
-                                        <div>
-                                            <label for="description" class="block text-sm font-medium text-gray-700">
-                                                Description
-                                            </label>
-                                            <div class="mt-1">
-                                                <textarea required id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md">{{ $video->description }}</textarea>
-                                            </div>
-                                            <p class="mt-2 text-sm text-gray-500">
-                                                Breu descripció del vídeo
-                                            </p>
-                                        </div>
+                <x-slot name="description">
+                    {{ __('Informació bàsica del video') }}
+                </x-slot>
 
-                                        <div class="grid grid-cols-3 gap-6">
-                                            <div class="col-span-3">
-                                                <label for="url" class="block text-sm font-medium text-gray-700">
-                                                    URL
-                                                </label>
-                                                <div class="mt-1 flex rounded-md shadow-sm">
+                <x-slot name="actions">
+                    <x-jet-button>
+                        {{ __('Update') }}
+                    </x-jet-button>
+                </x-slot>
+
+                <x-slot name="form">
+                    @csrf
+                    @method('PUT')
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-jet-label for="title" value="{{ __('Title') }}" />
+                        <x-jet-input id="title" name="title" type="text" class="mt-1 block w-full" autocomplete="name" required value="{{ $video->title }}"/>
+                        <x-jet-input-error for="title" class="mt-2" />
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-jet-label for="description" value="{{ __('Description') }}" />
+                        <textarea required id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Description">{{ $video->description }}</textarea>
+                        <x-jet-input-error for="description" class="mt-2" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-jet-label for="url" value="{{ __('Url') }}" />
+
+                        <div class="mt-1 flex rounded-md shadow-sm">
                                                           <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
                                                             http://
                                                           </span>
-                                                    <input required type="url" name="url" id="url" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block  rounded-none rounded-r-md sm:text-sm border-gray-300" value="{{$video->url}}">
-                                                </div>
-                                            </div>
-                                        </div>
-
-
-
-                                    </div>
-                                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                            Modificar
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
+                            <input required type="url" name="url" id="url" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block  rounded-none rounded-r-md sm:text-sm border-gray-300" value="{{$video->url}}">
                         </div>
+                        <x-jet-input-error for="url" class="mt-2" />
                     </div>
-                </div>
-            </div>
+                </x-slot>
+            </x-jet-form-section>
+
+            <x-jet-section-border />
+          
 
         @endcan
 
